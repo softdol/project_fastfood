51046532dbbebfb970695998aabbaed63487fdef
Gi7D-17 <Gi7D-17@DESKTOP-FVNHQFA>
2022-06-16 16:15:04 +0900

    로그인 테스트

--- fastfood/src/database/OjdbcConnection.java
+++ fastfood/src/database/OjdbcConnection.java
@@ -4,10 +4,11 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import database.model.PsInsert;
+import database.model.PsList;
 
 public class OjdbcConnection {
 	private static String driverName = "oracle.jdbc.driver.OracleDriver";
@@ -33,14 +34,14 @@ public class OjdbcConnection {
 	 * @param psList(I : number, S: varchar2, D:date)
 	 * @return
 	 */
-	public static boolean insert(String sql, ArrayList<PsInsert> psList) {	
+	public static boolean insert(String sql, ArrayList<PsList> psList) {	
 		try(
 				Connection conn = OjdbcConnection.getConnection();
 				PreparedStatement pstmt= conn.prepareStatement(sql);
 			){
 			
 			for(int i = 0; i < psList.size(); i++) {
-				PsInsert ps = psList.get(i); 
+				PsList ps = psList.get(i); 
 				switch(ps.getType()) {
 					case 'i': case 'I':
 						pstmt.setInt(i + 1, Integer.parseInt(ps.getVal()));
@@ -49,7 +50,6 @@ public class OjdbcConnection {
 						pstmt.setString(i + 1, ps.getVal());
 						break;
 					case 'd': case 'D':
-						//java.sql.Date.valueOf(LocalDate.now())
 						pstmt.setDate(i + 1, java.sql.Date.valueOf(ps.getVal()));
 						break;
 					default:
@@ -65,4 +65,97 @@ public class OjdbcConnection {
 			return false;
 		}
 	}
+	
+	/**
+	 * ArrayList 이용 DB select
+	 * @param sql
+	 * @param psList(I : number, S: varchar2, D:date)
+	 * @return ResultSet 리스트
+	 */	
+	public static ArrayList<ResultSet> select(String sql, ArrayList<PsList> psList) {
+		
+		ArrayList<ResultSet> rsList = new ArrayList<>();
+		
+		try(
+				Connection conn = OjdbcConnection.getConnection();
+				PreparedStatement pstmt= conn.prepareStatement(sql);
+			){
+			
+			for(int i = 0; i < psList.size(); i++) {
+				PsList ps = psList.get(i); 
+				switch(ps.getType()) {
+					case 'i': case 'I':
+						pstmt.setInt(i + 1, Integer.parseInt(ps.getVal()));
+						break;
+					case 's': case 'S':
+						pstmt.setString(i + 1, ps.getVal());
+						break;
+					case 'd': case 'D':
+						pstmt.setDate(i + 1, java.sql.Date.valueOf(ps.getVal()));
+						break;
+					default:
+						pstmt.setString(i + 1, ps.getVal());
+						break;				
+				}
+			}
+			
+			try(ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()){
+					rsList.add(rs);
+				}
+			}
+			
+			return rsList;
+						
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * ArrayList 이용 DB selectOne(단일)
+	 * @param sql
+	 * @param psList(I : number, S: varchar2, D:date)
+	 * @return ResultSet
+	 */	
+	public static ResultSet selectOne(String sql, ArrayList<PsList> psList) {
+		
+		try(
+				Connection conn = OjdbcConnection.getConnection();
+				PreparedStatement pstmt= conn.prepareStatement(sql);
+			){
+			
+			for(int i = 0; i < psList.size(); i++) {
+				PsList ps = psList.get(i); 
+				switch(ps.getType()) {
+					case 'i': case 'I':
+						pstmt.setInt(i + 1, Integer.parseInt(ps.getVal()));
+						break;
+					case 's': case 'S':
+						pstmt.setString(i + 1, ps.getVal());
+						break;
+					case 'd': case 'D':
+						pstmt.setDate(i + 1, java.sql.Date.valueOf(ps.getVal()));
+						break;
+					default:
+						pstmt.setString(i + 1, ps.getVal());
+						break;				
+				}
+			}
+			
+			try(ResultSet rs = pstmt.executeQuery()){
+				if(rs.next()){
+					return rs;
+				}
+			}			
+			return null;
+						
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
 }
---
+++ fastfood/src/database/manager/Member.java
@@ -0,0 +1,30 @@
+package database.manager;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Member {
+	
+	private Integer member_idx;
+	private Integer store_idx;
+	private String member_id;
+	private String member_name;
+	private String member_pass; 
+	  
+	private String MEMBER_USE_FLAG;   
+	private Date MEMBER_IN_DATE;
+	
+	public Member(ResultSet rs) throws SQLException {
+		
+		this.member_idx			= rs.getInt(1);
+		this.store_idx 			= rs.getInt(2);
+		this.member_id			= rs.getString(3);
+		this.member_name 		= rs.getString(4);
+		this.member_pass 		= rs.getString(5);		
+		this.MEMBER_USE_FLAG	= rs.getString(6);
+		this.MEMBER_IN_DATE		= rs.getDate(7);
+		
+	}
+	
+}
