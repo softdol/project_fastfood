562f5154549a65bfb40104d5fc366338afe08a52
Gi7D-17 <Gi7D-17@DESKTOP-FVNHQFA>
2022-06-23 10:58:23 +0900

    테스트

--- fastfood/src/manager/menu/MenuMainPanel.java
+++ fastfood/src/manager/menu/MenuMainPanel.java
@@ -3,7 +3,7 @@ package manager.menu;
 import javax.swing.JPanel;
 
 import manager.ManagerMain;
-import manager.member.MemberMain;
+import manager.sales.SalesMain;
 
 public class MenuMainPanel extends JPanel{
 	
@@ -24,10 +24,12 @@ public class MenuMainPanel extends JPanel{
 		jpMainMiddle.setBounds(0,0,1185,730);
 		//jpMainMiddle.add(new MenuInsertPanel(main.mInfo));
 		//jpMainMiddle.add(new MenuSetList(main));
-		jpMainMiddle.add(new MenuList(main));
+		//jpMainMiddle.add(new MenuList(main));
 		//jpMainMiddle.add(new MenuSetInsert(main));
 		//jpMainMiddle.add(new MenuSetModify(main, 1));
 		//jpMainMiddle.add(new MemberMain(main));
+		jpMainMiddle.add(new SalesMain(main));
+		
 		
 		add(jpMainMiddle);
 		
