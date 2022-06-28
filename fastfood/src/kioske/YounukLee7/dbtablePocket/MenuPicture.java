021863d9dfe12cc0cddec44452724cff439de470
Gi7D-09 <Gi7D-09@DESKTOP-E60JRQ6>
2022-06-28 16:36:36 +0900

    정보 넘겨주기 일부 완료

--- fastfood/src/kioske/YounukLee7/dbtablePocket/MenuPicture.java
+++ fastfood/src/kioske/YounukLee7/dbtablePocket/MenuPicture.java
@@ -5,12 +5,18 @@ import java.sql.SQLException;
 
 public class MenuPicture {
 
+	private Integer set_idx;
 	private String img_big_path;
 	private String set_img_path;
+	private String menu_name;
+	private String set_name;
 	
 	public MenuPicture(ResultSet rs) throws SQLException {
-		set_img_path = rs.getString(1);
-		img_big_path = rs.getString(2);
+		set_img_path = rs.getString("set_img_path");
+		img_big_path = rs.getString("img_big_path");
+		menu_name = rs.getString("menu_name");
+		set_name = rs.getString("set_name");
+		set_idx = rs.getInt("set_idx");
 	}
 	
 	
@@ -20,5 +26,17 @@ public class MenuPicture {
 	
 	public String getSet_img_path() {
 		return set_img_path;
+	}
+	
+	public String getMenu_name() {
+		return menu_name;
+	}
+	
+	public String getSet_name() {
+		return set_name;
+	}
+	
+	public Integer getSet_idx() {
+		return set_idx;
 	}
 }
