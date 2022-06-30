a742dfcd1656b80b7da4d2e2cf856f2c645bfa65
HYOJEONG\hyoje <jehun0905@naver.com>
2022-06-30 15:40:18 +0900

    Merge branch 'main' of https://github.com/softdol/project_fastfood.git into main

---
+++ fastfood/src/pos/ActionListener/ButtonActionListener.java
@@ -0,0 +1,51 @@
+package pos.ActionListener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+
+import database.manager.Menu;
+import pos.Order;
+import pos.Pos_Burger;
+
+public class ButtonActionListener implements ActionListener{
+	Menu m;
+	Pos_Burger main;
+	
+	public ButtonActionListener(Pos_Burger main, Menu m) {
+		this.main = main;
+		this.m = m;
+	
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//클릭했을 때 메뉴 정보 가져오기 
+	
+		JButton itemBtn = (JButton)e.getSource();
+		System.out.println(itemBtn.getText());
+		
+		String product = itemBtn.getText();
+		
+		// ORDERLIST에 메뉴리스트를 넣어라 
+		
+		main.orderlist.add(new Order(m,1,0));
+		
+//		for(Order o : main.orderlist) {
+//			System.out.println(o.getMenu_name() + " " + o.getMenu_idx());
+//		};
+		
+		main.setOrderlist();
+		//ArrayList<Order> orderlist = new ArrayList<>();
+		//main.orderlist.add(new Order(m,1,0));
+		
+		// orderlist 객체를 가지고, 테이블에 넣어라 (for문에 테이블 생성)해라 
+		
+		
+		
+	}
+
+	
+}
---
+++ fastfood/src/pos/MenuListPanel.java
@@ -0,0 +1,68 @@
+package pos;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import database.manager.Menu;
+import database.manager.ReturnModel;
+import database.model.PsList;
+import pos.ActionListener.ButtonActionListener;
+
+public class MenuListPanel extends JPanel{
+	Pos_Burger main;
+	
+	public MenuListPanel(int iCate,Pos_Burger main) {
+		this.main = main;
+		setBackground(new Color(0xD9EDDF));
+		setBounds(400,120,455,460);
+		
+		String sql = "select * from menu where menu_category_idx = ?";
+		ArrayList<PsList> psList = new ArrayList<>();
+		psList.add(new PsList('I', String.valueOf(iCate)));
+		
+		
+		ArrayList<Menu> menuList = ReturnModel.selMenuList(sql, psList);
+		JButton itemBtn;
+		
+		int maxItem = Math.max(menuList.size(), 12);
+		setLayout(new GridLayout(maxItem/3 + 1, 3));
+		
+		for(int i  = 0; i < maxItem; i++) {
+			
+			if(i < menuList.size()) {
+				Menu m = menuList.get(i);
+				
+				itemBtn = new JButton();
+				itemBtn.setBounds(100, 100, 10, 10);
+				itemBtn.setPreferredSize(new Dimension(120, 80));
+				itemBtn.setText(m.getMenu_name());
+				itemBtn.addActionListener(new ButtonActionListener(main,m));
+				add(itemBtn);
+			
+				
+				
+			}else {
+				itemBtn = new JButton();
+				itemBtn.setBounds(100, 100, 10, 10);
+				itemBtn.setPreferredSize(new Dimension(120, 80));
+				itemBtn.setText("ÁØºñÁß");
+				add(itemBtn);
+			}
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+
+}
